126abb9342aad3be61d07cf7ef00eac1c1b8be8b
jtilson <jtilson@f8f189e1-3a9a-43af-a5cf-3c110a5698ba>
2010-11-10 18:19:05 +0000

    Added admin stats section

---
+++ views/default/todo/admin/css.php
@@ -0,0 +1,57 @@
+<?php
+	/**
+	 * Todo Admin CSS
+	 * 
+	 * @package Todo
+	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+	 * @author Jeff Tilson
+	 * @copyright THINK Global School 2010
+	 * @link http://www.thinkglobalschool.com/
+	 * 
+	 */
+?>
+<style type="text/css">
+
+.todoadmin {
+	
+}
+
+.todoadmin table.todostats {
+	border: 1px solid #ccc;
+	margin-top: 15px;
+	width: 60%;
+}
+
+.todoadmin table.todostats tr:nth-child(odd) { 
+	background-color:#eee; 
+}
+
+.todoadmin table.todostats tr:nth-child(even) { 
+	background-color:#fff;
+}
+
+.todoadmin table.todostats caption {
+	font-size:1.2em;
+	line-height:1.0em;
+	color: #666666;
+	font-weight: bold;
+	margin-bottom: 5px;
+}
+
+.todoadmin table.todostats td {
+	padding: 5px;
+}
+
+.todoadmin table.todostats td.label {
+	font-weight: bold;
+	width: 50%;
+	border-right: 1px solid #ddd;
+}
+
+.todoadmin table.todostats td.content {
+	padding-left: 12px;
+}
+
+
+
+</style>
