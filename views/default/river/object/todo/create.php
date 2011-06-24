9904b83da6820704ef2ae42e4229b079f386cb85
jtilson <jtilson@f8f189e1-3a9a-43af-a5cf-3c110a5698ba>
2011-06-24 17:20:11 +0000

    Update river views. Fixed bad redirect when deleting a todo

--- views/default/river/object/todo/create.php
+++ views/default/river/object/todo/create.php
@@ -11,25 +11,10 @@
  */
 
 $object = $vars['item']->getObjectEntity();
+$excerpt = strip_tags($object->description);
+$excerpt = elgg_get_excerpt($excerpt);
 
-$params = array(
-	'href' => $object->getURL(),
-	'text' => $object->title,
-);
-
-$link = elgg_view('output/url', $params);
-
-$group_string = '';
-$container = $object->getContainerEntity();
-if ($container instanceof ElggGroup) {
-	$params = array(
-		'href' => $container->getURL(),
-		'text' => $container->name,
-	);
-	$group_link = elgg_view('output/url', $params);
-	$group_string = elgg_echo('todo:ingroup', array($group_link));
-}
-
-echo elgg_echo('todo:river:create');
-
-echo " $link $group_string";
+echo elgg_view('river/item', array(
+	'item' => $vars['item'],
+	'message' => $excerpt,
+));
