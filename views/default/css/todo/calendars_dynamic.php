a8a587aaaaa0b1ccfb0a96b59c154762530cdb6d
Jeff Tilson <jtilson@thinkglobalschool.com>
2012-10-03 13:12:57 -0400

    Missed calendar css in last commit

---
+++ views/default/css/todo/calendars_dynamic.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Todo Dynamic Calendar CSS (will not cache)
+ * 
+ * @package Todo
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010
+ * @link http://www.thinkglobalschool.com/
+ *
+ */
+header('Content-type: text/css', true);
+
+$calendar_colors = elgg_get_plugin_setting('calendar_category_colors', 'todo');
+$calendar_colors = unserialize($calendar_colors);
+
+foreach ($calendar_colors as $guid => $calendar) {
+	$bg = $calendar['bg'];
+	$fg = $calendar['fg'];
+	
+echo <<<___CSS
+	.elgg-todocalendar-feed-$guid a,
+	.elgg-todocalendar-feed-$guid,
+	.elgg-todocalendar-feed-$guid .fc-event-skin {
+		background-color: #$bg;
+		border-color: #$bg;
+		color: #$fg
+	}
+___CSS;
+}
